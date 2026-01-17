84da0f7ee34bacb439107cb0db73b16f31576087
Nathan Slone <NathanSlone2010@icloud.com>
2026-01-17 17:29:34 -0500

    Enhance Celsius converter with welcome message
    Added welcome message and author information to the Celsius converter.

--- Celsiusconv.cpp
+++ Celsiusconv.cpp
@@ -2,10 +2,13 @@
 
 using namespace std;
 int main() {
-    cout << "Celsius: ";
+    cout << "Welcome to the Celsius Converter!";
+    cout << "\n Made by Nathan T. Slone";
+    cout << "\n Celsius: ";
     int celsius;
     cin >> celsius;
     double fahrenheit = (celsius * 9.0) / 5.0 + 32;
     cout << "Fahrenheit: " << fahrenheit;
+    cout << "Please restart the program to input another number.";
     return 0;
 }
